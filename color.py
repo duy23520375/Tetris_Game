32ed15bb54443983c94cd2e4fd96e072a280fc86
duy23520375 <164975720+duy23520375@users.noreply.github.com>
2024-06-08 22:42:56 +0700

    Update color.py

--- color.py
+++ color.py
@@ -1,4 +1,19 @@
 class Colors:
+    """
+        Định nghĩa các màu sắc được sử dụng cho các khối Tetris.
+        Attributes:
+            dark_gray (tuple): Mã màu RGB cho màu đen xám.
+            green (tuple): Mã màu RGB cho màu xanh lá cây.
+            red (tuple): Mã màu RGB cho màu đỏ.
+            orange (tuple): Mã màu RGB cho màu cam.
+            yellow (tuple): Mã màu RGB cho màu vàng.
+            purple (tuple): Mã màu RGB cho màu tím.
+            cyan (tuple) : Mã màu RGB cho màu lục lam
+            blue (tuple): Mã màu RGB cho màu xanh dương.
+            dark_blue (tuple): Mã màu RGB cho màu xanh đen.
+        Method:
+            get_cell_colors()
+        """
     dark_gray = (26, 31, 40)
     green = (47, 230, 23)
     red = (232, 18, 18)
@@ -10,4 +25,9 @@ class Colors:
     dark_blue = (44, 44, 127)
     @classmethod
     def get_cell_colors(cls):
+        """
+            Danh sách các màu sắc của các ô khối.
+            Return:
+                list: Danh sách các mã màu RGB của các ô khối.
+                """
         return [cls.dark_gray,cls.green,cls.red,cls.orange,cls.yellow,cls.purple,cls.cyan,cls.blue]
