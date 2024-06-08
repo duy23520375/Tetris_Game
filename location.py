6a62ac37e23e2af9f94d2af6540dc880d823eb57
duy23520375 <164975720+duy23520375@users.noreply.github.com>
2024-06-08 22:43:53 +0700

    Update location.py

--- location.py
+++ location.py
@@ -1,4 +1,10 @@
-class Possition:
+class Position:
+    """
+       Lớp đại diện cho vị trí của một khối hoặc ô trong lưới.
+       Attributes:
+           row (int): Vị trí hàng.
+           col (int): Vị trí cột.
+       """
     def __init__(self,row,col):
         self.row = row
         self.col = col
